613b47acab60742c17e21426fc1fb52e4c4f56d7
Luxgile <bisslit.12.2@gmail.com>
2023-10-13 16:41:45 +0200

    Modified bootstrap logic to work out the box for Linux

--- init.vim
+++ init.vim
@@ -36,6 +36,11 @@ nnoremap <leader>tw <cmd>exe v:count1 . "ToggleTerm"<cr>
 nnoremap <leader>tc <cmd>exe v:count1 . "ToggleTerm dir=%:h"<cr>
 nnoremap <leader>ss <cmd>Telescope possession list<cr>
 
+let data_dir = has('nvim') ? stdpath('data') . '/site' : '~/.vim'
+if empty(glob(data_dir . '/autoload/plug.vim'))
+  silent execute '!curl -fLo '.data_dir.'/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+endif
 
 " Vim Plug init.
 call plug#begin('~/.config/nvim/plugged')
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,6 +8,19 @@ function _G.set_terminal_keymaps()
   vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
 end
 
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
   vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
