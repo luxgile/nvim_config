ba202ab92e249f3ff6b5b81a14e67a021e979bae
luxgile <bisslit.12.2@gmail.com>
2023-10-19 01:01:16 +0200

    Added mostly everything basic

--- init.lua
+++ init.lua
@@ -3,19 +3,29 @@
 vim.keymap.set('n', '<F10>', ':e ~/AppData/Local/nvim/init.lua<CR>')
 
 vim.g.mapleader = ' '
+vim.opt.timeoutlen = 3000
+vim.opt.ttimeoutlen = 100
 
 vim.opt.background = "dark"
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.completeopt = {'noinsert', 'menuone', 'noselect'}
+vim.opt.shortmess = vim.opt.shortmess + { c = true }
+vim.api.nvim_set_option('updatetime', 300)
 vim.opt.number = true
 vim.opt.hidden = true
 vim.opt.relativenumber = true
 vim.opt.title = true
 vim.opt.autoindent = true
+vim.opt.encoding = 'utf-8'
 
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+
+-- GENERAL REMAPPINGS
+vim.keymap.set('n', '<Leader>ff', '<CMD>Telescope find_files<CR>')
+vim.keymap.set('n', '<Leader>b', '<CMD>Telescope buffers<CR>')
+vim.keymap.set('n', '<Leader>fg', '<CMD>Telescope live_grep<CR>')
 
 -- LAZY VIM BOOTSTRAP
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -33,5 +43,166 @@ vim.opt.rtp:prepend(lazypath)
 
 -- LAZY VIM SETUP
 require('lazy').setup({
+  { 'nvim-lua/plenary.nvim' },
+  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  { 'nvim-telescope/telescope.nvim', tag = '0.1.4'},
+  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+  { 'nvim-tree/nvim-web-devicons' },
+  { 'simrat39/rust-tools.nvim' },
+  { 'neovim/nvim-lspconfig' },
+  { 'nvim-lualine/lualine.nvim' },
+  { "williamboman/mason.nvim",
+    lazy = false,
+    opts = {},
+  },
+  { 'williamboman/mason-lspconfig.nvim',
+    lazy = false,
+    opts = {},
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    opts = {} -- this is equalent to setup({}) function
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function(plugin)
+      vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
+      vim.cmd([[colorscheme tokyonight]])
+    end
+  },
+    -- Completion framework:
+  { 'hrsh7th/nvim-cmp' },
 
+    -- LSP completion source:
+  { 'hrsh7th/cmp-nvim-lsp' },
+
+    -- Useful completion sources:
+  {'hrsh7th/cmp-nvim-lua'},
+   {'hrsh7th/cmp-nvim-lsp-signature-help'},
+   {'hrsh7th/cmp-vsnip'},
+     {'hrsh7th/cmp-path'},
+     {'hrsh7th/cmp-buffer'},
+     {'hrsh7th/vim-vsnip'},
+})
+
+-- LUA LINE SETUP
+require('lualine').setup {
+  options = {
+    theme = 'tokyonight'
+  }
+}
+
+-- TREESITTER SETUP
+require('nvim-treesitter.configs').setup {
+  ensure_installed = { "lua", "rust", "toml" },
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting=false,
+  },
+  ident = { enable = true }, 
+  rainbow = {
+    enable = true,
+    extended_mode = true,
+    max_file_lines = nil,
+  }
+}
+
+-- RUST TOOLS SETUP
+require("rust-tools").setup ({
+  server = {
+    on_attach = function(_, bufnr)
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  }
+})
+
+-- LSP Diagnostics Options Setup 
+local sign = function(opts)
+  vim.fn.sign_define(opts.name, {
+    texthl = opts.name,
+    text = opts.text,
+    numhl = ''
+  })
+end
+
+sign({name = 'DiagnosticSignError', text = ''})
+sign({name = 'DiagnosticSignWarn', text = ''})
+sign({name = 'DiagnosticSignHint', text = ''})
+sign({name = 'DiagnosticSignInfo', text = ''})
+
+vim.diagnostic.config({
+    virtual_text = false,
+    signs = true,
+    update_in_insert = true,
+    underline = true,
+    severity_sort = false,
+    float = {
+        border = 'rounded',
+        source = 'always',
+        header = '',
+        prefix = '',
+    },
+})
+
+vim.cmd([[
+set signcolumn=yes
+autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
+
+-- AUTOCOMPLETE SETUP
+local cmp = require'cmp'
+cmp.setup({
+  -- Enable LSP snippets
+  snippet = {
+    expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    -- Add tab support
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<C-S-f>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    })
+  },
+  -- Installed sources:
+  sources = {
+    { name = 'path' },                              -- file paths
+    { name = 'nvim_lsp', keyword_length = 3 },      -- from language server
+    { name = 'nvim_lsp_signature_help'},            -- display function signatures with current parameter emphasized
+    { name = 'nvim_lua', keyword_length = 2},       -- complete neovim's Lua runtime API such vim.lsp.*
+    { name = 'buffer', keyword_length = 2 },        -- source current buffer
+    { name = 'vsnip', keyword_length = 2 },         -- nvim-cmp source for vim-vsnip 
+    { name = 'calc'},                               -- source for math calculation
+  },
+  window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+  },
+  formatting = {
+      fields = {'menu', 'abbr', 'kind'},
+      format = function(entry, item)
+          local menu_icon ={
+              nvim_lsp = 'λ',
+              vsnip = '⋗',
+              buffer = 'Ω',
+              path = '🖫',
+          }
+          item.menu = menu_icon[entry.source.name]
+          return item
+      end,
+  },
 })
