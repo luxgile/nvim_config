eccab3daef584e7976c0a78373687e19bc2acbda
luxgile <bisslit.12.2@gmail.com>
2024-09-04 18:52:19 +0200

    Added rustaceanvim

--- lua/rust.lua
+++ lua/rust.lua
@@ -2,54 +2,59 @@ local M = {}
 
 local p = {
   { 'simrat39/rust-tools.nvim' },
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^5', -- Recommended
+    lazy = false,   -- This plugin is already lazy
+  }
 }
 
 function init()
   -- RUST TOOLS SETUP
   -- Workaround to external files being added to workspace
-  local function ensure_uri_scheme(uri)
-    if not vim.startswith(uri, "file://") then
-      return "file://" .. uri
-    end
-    return uri
-  end
-
-  local function is_in_workspace(uri)
-    uri = ensure_uri_scheme(uri)
-    local path = vim.uri_to_fname(uri)
-    local workspace_dir = vim.fn.getcwd()
-
-    return vim.startswith(path, workspace_dir)
-  end
-
-  local function filter_diagnostics(diagnostics)
-    local filtered_diagnostics = {}
-    for _, diagnostic in ipairs(diagnostics) do
-      if is_in_workspace(diagnostic.source) then
-        table.insert(filtered_diagnostics, diagnostic)
-      end
-    end
-    return filtered_diagnostics
-  end
-
+  -- local function ensure_uri_scheme(uri)
+  --   if not vim.startswith(uri, "file://") then
+  --     return "file://" .. uri
+  --   end
+  --   return uri
+  -- end
+  --
+  -- local function is_in_workspace(uri)
+  --   uri = ensure_uri_scheme(uri)
+  --   local path = vim.uri_to_fname(uri)
+  --   local workspace_dir = vim.fn.getcwd()
+  --
+  --   return vim.startswith(path, workspace_dir)
+  -- end
+  --
+  -- local function filter_diagnostics(diagnostics)
+  --   local filtered_diagnostics = {}
+  --   for _, diagnostic in ipairs(diagnostics) do
+  --     if is_in_workspace(diagnostic.source) then
+  --       table.insert(filtered_diagnostics, diagnostic)
+  --     end
+  --   end
+  --   return filtered_diagnostics
+  -- end
+  --
   local lspconfig = require 'lspconfig'
-  lspconfig.rust_analyzer.setup {
-    root_dir = require "lspconfig/util".root_pattern("Cargo.toml"),
-    -- Server-specific settings. See `:help lspconfig-setup`
-    settings = {
-      ['rust-analyzer'] = {},
-    },
-  }
-
-  local rt = require("rust-tools")
-  rt.setup({
-    server = {
-      on_attach = function(_, bufnr)
-        vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-        vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-      end,
-    }
-  })
+  -- lspconfig.rust_analyzer.setup {
+  --   root_dir = require "lspconfig/util".root_pattern("Cargo.toml"),
+  --   -- Server-specific settings. See `:help lspconfig-setup`
+  --   settings = {
+  --     ['rust-analyzer'] = {},
+  --   },
+  -- }
+  --
+  -- local rt = require("rust-tools")
+  -- rt.setup({
+  --   server = {
+  --     on_attach = function(_, bufnr)
+  --       vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+  --       vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+  --     end,
+  --   }
+  -- })
 
   -- WGSL SETUP
   vim.cmd("au BufNewFile,BufRead *.wgsl set filetype=wgsl")
