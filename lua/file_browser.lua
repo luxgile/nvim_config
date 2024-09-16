b030dde674e82f707499425d24c580ff2ee911dd
luxgile <bisslit.12.2@gmail.com>
2024-09-16 22:42:59 +0200

    Added picker and improved sessions and file finding

--- lua/file_browser.lua
+++ lua/file_browser.lua
@@ -4,6 +4,7 @@ local wk = require("which-key")
 -- Generic picker
 add('echasnovski/mini.pick')
 require('mini.pick').setup()
+vim.ui.select = MiniPick.ui_select
 
 -- Oil file browser
 -- add( 'stevearc/oil.nvim' )
@@ -17,7 +18,9 @@ require('mini.files').setup({
     go_out = '<Left>',
   },
 })
+
 wk.add({
   { "<leader>f", group = "Files" },
-  { "<leader>ft", "<cmd>lua MiniFiles.open()<cr>", desc = "File tree" }
+  { "<leader>ft", "<cmd>lua MiniFiles.open()<cr>", desc = "File tree" },
+  { "<leader>ff", "<cmd>Pick files<cr>", desc = "Find file" },
 })
--- lua/sessions.lua
+++ lua/sessions.lua
@@ -1,7 +1,15 @@
 local add = MiniDeps.add
+local wk = require('which-key')
 
 add('echasnovski/mini.starter')
 require('mini.starter').setup()
 
 add('echasnovski/mini.sessions')
-require('mini.sessions').setup()
+require('mini.sessions').setup({
+  autoread = true,
+  autowrite = true,
+})
+wk.add({
+  { "<leader>pp", "<cmd>lua MiniSessions.select()<cr>", desc = "Select session" },
+})
+
