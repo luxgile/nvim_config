0a651ae6384ad840d12c5bb6843e156b5a22372b
luxgile <bisslit.12.2@gmail.com>
2024-09-17 16:49:35 +0200

    Added easy session saving

--- lua/sessions.lua
+++ lua/sessions.lua
@@ -10,6 +10,12 @@ require('mini.sessions').setup({
   autowrite = true,
 })
 wk.add({
+  { "<leader>p", group = "Sessions" },
   { "<leader>pp", "<cmd>lua MiniSessions.select()<cr>", desc = "Select session" },
+  { "<leader>ps", function() save_session() end, desc = "Save session" },
 })
 
+function save_session ()
+ local user_input = vim.fn.input("Enter input: ")
+ MiniSessions.write(user_input)
+end
