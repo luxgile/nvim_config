e2a8b695468f820b04be523083b580c9644cdc44
luxgile <bisslit.12.2@gmail.com>
2025-01-22 22:52:17 +0100

    Updated lsp config for better diagnostics

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -53,6 +53,37 @@ vim.api.nvim_create_autocmd('LspAttach', {
     })
   end
 })
+-- Diagnostic float window
+vim.api.nvim_create_autocmd("CursorHold", {
+  buffer = bufnr,
+  callback = function()
+    local opts = {
+      focusable = false,
+      close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+      border = "rounded",
+      source = "always",
+      prefix = " ",
+      scope = "cursor",
+    }
+    vim.diagnostic.open_float(nil, opts)
+  end
+})
+-- Change diagnostic symbols
+local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+-- Change diagnostic display
+vim.diagnostic.config({
+  virtual_text = false,
+  signs = true,
+  underline = true,
+  update_in_insert = true,
+  severity_sort = false,
+})
+
 
 -- DAP
 add('mfussenegger/nvim-dap')
