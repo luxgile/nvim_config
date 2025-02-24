35a7e726b2fe6c0f5d2c8b359f531ffc8e0b3778
luxgile <bisslit.12.2@gmail.com>
2025-02-25 00:04:15 +0100

    Dap for cpp

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -366,3 +366,23 @@ lspconfig.clangd.setup({
 })
 
 lspconfig.neocmake.setup({})
+for _, lang in ipairs({ "c", "cpp" }) do
+  dap.configurations[lang] = {
+    {
+      type = "codelldb",
+      request = "launch",
+      name = "Launch file",
+      program = function()
+        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+      end,
+      cwd = "${workspaceFolder}",
+    },
+    {
+      type = "codelldb",
+      request = "attach",
+      name = "Attach to process",
+      pid = require("dap.utils").pick_process,
+      cwd = "${workspaceFolder}",
+    },
+  }
+end
