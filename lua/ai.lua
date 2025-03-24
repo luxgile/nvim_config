b0dda5406968770ec2272279d7c4097c769268a6
luxgile <bisslit.12.2@gmail.com>
2025-03-24 19:31:01 +0100

    Removed AI

--- lua/ai.lua
+++ lua/ai.lua
@@ -1,17 +1,18 @@
 local add = MiniDeps.add
 local wk = require("which-key")
 
+-- TODO: Swap to the previous version. This one is very limited.
 add({
   source = 'Exafunction/codeium.nvim',
   depends = {'nvim-lua/plenary.nvim'}
 })
 
-require('codeium').setup({
-  enable_cmp_source = false,
-  virtual_text = {
-    enabled = true,
-  }
-})
+-- require('codeium').setup({
+--   enable_cmp_source = false,
+--   virtual_text = {
+--     enabled = true,
+--   }
+-- })
 -- vim.g.codeium_disable_bindings = 1
 -- wk.add({
 --   { "<leader>i", group = "AI" },
