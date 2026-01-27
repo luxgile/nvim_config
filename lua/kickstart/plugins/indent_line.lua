cfa2252ebe0995b2277d92893d613ae08aae548e
luxgile <bisslit.12.2@gmail.com>
2026-01-27 15:22:07 +0100

    customized indent blank line

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -4,6 +4,11 @@ return {
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
     main = 'ibl',
-    opts = {},
+    opts = {
+      whitespace = {
+        highlight = { 'Function', 'Label' },
+        remove_blankline_trail = true,
+      },
+    },
   },
 }
