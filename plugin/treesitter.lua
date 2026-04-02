f08a5128224e48513bffcb1e2469ad9212cc2d17
luxgile <bisslit.12.2@gmail.com>
2026-04-03 00:16:13 +0200

    tried updating treesitter, no difference was made

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -1,4 +1,6 @@
-vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter" })
+vim.pack.add({
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
+})
 
 require("nvim-treesitter").setup({
 	ensure_installed = {
